3e50686a1c13fe8bb2888e67ff6ac28c6c4610c3
twjw <aaaawjwa@gmail.com>
2024-02-07 10:03:36 +0800

    remove nest

--- js/nest-project/src/metadata.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable */
-export default async () => {
-    const t = {
-        ["./app.response"]: await import("./app.response")
-    };
-    return { "@nestjs/swagger": { "models": [[import("./app.response"), { "User": { id: { required: true, type: () => Number }, name: { required: true, type: () => String }, age: { required: true, type: () => Number } } }]], "controllers": [[import("./app.controller"), { "AppController": { "getHello": { description: "123", type: String }, "getUser": { type: t["./app.response"].User } } }]] } };
-};
